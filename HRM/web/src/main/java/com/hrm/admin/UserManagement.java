7d746f053d780e28d7070d8f12a5967c660e4ee0
sebbatko <sbatko@student.agh.edu.pl>
2015-01-07 13:42:55 +0100

    panelusera

---
+++ HRM/web/src/main/java/com/hrm/admin/UserManagement.java
@@ -0,0 +1,26 @@
+package com.hrm.admin;
+
+import java.util.List;
+
+import com.hrm.DaoInitializer;
+import com.hrm.db.dao.AdminDao;
+import com.hrm.db.model.User;;
+
+public class UserManagement {
+	private AdminDao adminDao;
+	
+	
+	public UserManagement(){
+		adminDao = DaoInitializer.getDao(AdminDao.class);
+	}
+	
+	public void show(){
+		
+		List<User> userList = adminDao.getAllUsers();
+		
+		System.out.println("pierwszy: " + userList.get(0));
+		System.out.println("ilość: " + userList.size());
+	}
+	
+	
+}
