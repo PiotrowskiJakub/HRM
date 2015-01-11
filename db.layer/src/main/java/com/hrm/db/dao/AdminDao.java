8eb8d271309a064505da74602abd1d2b9e6724ce
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2015-01-11 17:14:26 +0100

    Dodanie uslug do AdminDao

--- db.layer/src/main/java/com/hrm/db/dao/AdminDao.java
+++ db.layer/src/main/java/com/hrm/db/dao/AdminDao.java
@@ -14,7 +14,10 @@ public interface AdminDao
 	List<User> getAllUsers();
 	List<Role> getAllRoles();
 	List<Section> getAllSection();
-	Role getRole(String code);
+	Role getRoleByCode(String code);
+	Role getRoleByName(String name);
+	Section getSectionByCode(String code);
+	Section getSectionByName(String name);
 	void addUser(User user);
 	void deleteUser(String login);
 	User getUser(String login);
