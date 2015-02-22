dab79cc07550558cccd3a2ab1825be5ee614a0a4
sebbatko <sbatko@student.agh.edu.pl>
2015-02-22 01:56:38 +0100

    dodane wyswietlanie listy aktywnosci usera

---
+++ HRM/web/src/main/java/com/hrm/admin/WorkLogAdapter.java
@@ -0,0 +1,30 @@
+package com.hrm.admin;
+
+import java.util.Date;
+
+import com.hrm.db.model.WorkLog;
+
+public class WorkLogAdapter implements UserRegistration {
+	WorkLog ref;
+	
+	public WorkLogAdapter(WorkLog ref){
+		this.ref = ref;
+	}
+	
+	@Override
+	public String getRegistrationTitle() {
+		return "Nazwa projektu: <b>" + ref.getTask().getProject().getPrjName() + "</b><br />Nazwa zadania: <b>" + ref.getTask().getNazwa() + "</b><br /> Data wpisu: <b>" + ref.getWloDate().toString() + "</b><br />Czas pracy: <b>" + UserManagement.getTimeOutput(ref.getWloTime()) +"</b>";
+		
+	}
+
+	@Override
+	public String getContent(){
+		return "<b>Wykonana praca:</b><br />" + ref.getWloComment();
+	}
+	
+	@Override
+	public Date getCreateDate(){
+		return ref.getWloDate();
+	}
+	
+}
